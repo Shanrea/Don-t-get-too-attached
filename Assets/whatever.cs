8002f9a2f5fd9a403e6fb972a5f275dad872b08a
Shanrea <96248027+Shanrea@users.noreply.github.com>
2021-12-16 15:41:33 +0100

    Update whatever.cs

--- Assets/whatever.cs
+++ Assets/whatever.cs
@@ -7,7 +7,7 @@ public class whatever : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //comment
     }
 
     // Update is called once per frame
